47d8ab4a44611c245f86ce4fb5c7a410570cc7de
VanthuirMaia <vanmaiasf@gmail.com>
2025-03-03 18:31:25 -0300

    Atividades Inteiros

---
+++ exercicios_Int.py
@@ -0,0 +1,33 @@
+
+# Atividades Inteiros (int)
+
+# escreva um programa que soma dois numerosinteiros iseridos pelo usuario
+
+num_1 = int(input('Digite um numero:   '))
+num_2 = int(input('Digite um numero:   '))
+soma = num_1 + num_2
+print(f'A soma dos numeros é: {soma}')
+
+# Crie um programa que receba um numero do usuario e calcule o resto da divisão desse número por 5.
+num_1 = int(input('Digite um numero:   '))
+resto = num_1 % 5
+print(f'O resto da divisão por 5 é: {resto}')
+
+# Desenvolva um programa que multiplique dois numeros fornecidos pelo usuario e mostre o resultado
+
+num_1 = int(input('Digite um numero:   '))
+num_2 = int(input('Digite um numero:   '))
+multiplicacao = num_1 * num_2
+print(f'A multiplicação dos numeros é: {multiplicacao}')
+
+# Faça um programa que peça dois numeros inteiros e imprima a divisão inteira do primeiro pelo segundo
+num_1 = int(input('Digite um numero:   '))
+num_2 = int(input('Digite um numero:   '))
+divisao = num_1 // num_2
+print(f'A divisão inteira dos numeros é: {divisao}')
+
+# Escreva um programa que calcule o quadrado de um número fornecido pelo usuario
+num_1 = int(input('Digite um numero:   '))
+quadrado = num_1 ** 2
+print(f'O quadrado do numero é: {quadrado}')
+
